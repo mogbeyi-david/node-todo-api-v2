c03a3c84118527f47ae112f3377424d910907db6
DavidMogbeyi <davidm@l5lab.com>
2019-02-22 07:59:50 +0100

    Pulled the user model into the user routes file

--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,7 @@ const bcrypt = require('bcrypt')
 
 const router = express.Router()
 const validateUser = require('../validation/user')
+const User = require('../models/user');
 
 router.post('/', function (req, res) {
   res.status(httpStatusCodes.OK).send('Good to go');
