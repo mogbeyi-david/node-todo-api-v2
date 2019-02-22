5ca2e2c48a82eaebbe865139cc38425537e4a58e
DavidMogbeyi <davidm@l5lab.com>
2019-02-22 07:37:39 +0100

    Installed bcrypt

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const express = require('express');
+const httpStatusCodes = require('http-status-codes');
 
 const router = express.Router();
 const validateUser = require('../validation/user');
