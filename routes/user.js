85d261b9255e991c119f8c7451512643f9033659
DavidMogbeyi <davidm@l5lab.com>
2019-02-22 10:21:32 +0100

    Blocked ablility to create duplicate users

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,4 @@
-const _ = require('lodash');
+const _ = require('lodash')
 const mongoose = require('mongoose')
 const express = require('express')
 const httpStatusCodes = require('http-status-codes')
@@ -11,6 +11,8 @@ const User = require('../models/user')
 router.post('/signup', async function (req, res) {
   const {error, value} = validateUser(req.body)
   if (error) res.status(httpStatusCodes.BAD_REQUEST).send({message: error.details[0].message, data: value})
+  const user = await User.find({email: req.body.email})
+  if (user) res.status(httpStatusCodes.BAD_REQUEST).send({message: 'User already exists', data: req.body})
   const SALT_FACTOR = await bcrypt.genSalt(10)
   const hashedPassword = await bcrypt.hash(req.body.password, SALT_FACTOR)
   const newUser = new User({
@@ -18,8 +20,8 @@ router.post('/signup', async function (req, res) {
     email: req.body.email,
     password: hashedPassword
   })
-  const user = await newUser.save()
-  const response = _.pick(user, ['_id', 'name', 'email'])
+  const result = await newUser.save()
+  const response = _.pick(result, ['_id', 'name', 'email'])
   res.status(httpStatusCodes.CREATED).send(response)
 })
 
