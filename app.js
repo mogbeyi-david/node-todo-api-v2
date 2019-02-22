8a286904170408f69401e80f300019f4e7a59406
DavidMogbeyi <davidm@l5lab.com>
2019-02-22 07:06:52 +0100

    code cleanup

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
 const mongoose = require('mongoose')
-const express = require('express');
+const express = require('express')
+const app = express()
 require('dotenv').config() // pull in dotenv to access enviroment variables
 
 // Declare environment variables
 const database = process.env.DATABASE
 const hostname = process.env.HOSTNAME
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
 //Try to connect to the database
 mongoose.connect(`mongodb://${hostname}/${database}`, {useNewUrlParser: true})
   .then(() => {console.log('Connected to Mongodb successfully')})
   .catch((error) => {console.error('Could not connect to Mongo DB: ', error)})
 
-const app = express();
-
-
-app.listen(()=>{
+//Set app to listen on PORT
+app.listen(PORT, () => {
   console.log(`App started and listening on port ${PORT}`)
 })
