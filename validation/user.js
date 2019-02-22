47f762fda7929162c500595226396b22d53ba2ae
DavidMogbeyi <davidm@l5lab.com>
2019-02-22 08:10:52 +0100

    Updated the joi password validation to take minlength and maxlength

--- validation/user.js
+++ validation/user.js
@@ -4,7 +4,7 @@ function validateUser (user) {
   const schema = Joi.object().keys({
     name: Joi.string().required(),
     email: Joi.string().required().email,
-    password: Joi.string().required().regex(/^[a-zA-Z0-9]{3,30}$/)
+    password: Joi.string().required().min(6).max(255).regex(/^[a-zA-Z0-9]{3,30}$/)
   });
   return Joi.validate(user, schema);
 }
