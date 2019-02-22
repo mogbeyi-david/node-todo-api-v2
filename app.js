da6f0ce123f27cfdc5791ab621575fe48a4988dc
DavidMogbeyi <davidm@l5lab.com>
2019-02-22 07:03:20 +0100

    Setup port for listening

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const mongoose = require('mongoose')
+const express = require('express');
 require('dotenv').config() // pull in dotenv to access enviroment variables
 
-//Try to connect to the database
+// Declare environment variables
 const database = process.env.DATABASE
 const hostname = process.env.HOSTNAME
+const PORT = process.env.PORT || 3000;
+
+//Try to connect to the database
 mongoose.connect(`mongodb://${hostname}/${database}`, {useNewUrlParser: true})
   .then(() => {console.log('Connected to Mongodb successfully')})
   .catch((error) => {console.error('Could not connect to Mongo DB: ', error)})
+
+const app = express();
+
+
+app.listen(()=>{
+  console.log(`App started and listening on port ${PORT}`)
+})
