4abe77641d7ee229df859ae0dc6c40b4b9e5cd84
DavidMogbeyi <davidm@l5lab.com>
2019-02-22 08:57:09 +0100

    implement the sign up method

--- routes/user.js
+++ routes/user.js
@@ -7,10 +7,18 @@ const router = express.Router()
 const validateUser = require('../validation/user')
 const User = require('../models/user')
 
-router.post('/', function (req, res) {
+router.post('/', async function (req, res) {
   const {error, value} = validateUser(req.body)
-  if (error) res.status(httpStatusCodes.BAD_REQUEST).send({message: error, data: value})
-  res.send(req.body)
+  if (error) res.status(httpStatusCodes.BAD_REQUEST).send({message: error.details[0].message, data: value})
+  const SALT_FACTOR = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(req.body.password, SALT_FACTOR)
+  const newUser = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: hashedPassword
+  })
+  const user = await newUser.save()
+  res.status(httpStatusCodes.CREATED).send(user)
 })
 
 module.exports = router
