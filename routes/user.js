825f5ed8f89372f5c16bcb9d9782ddc756e417f8
DavidMogbeyi <davidm@l5lab.com>
2019-02-22 07:35:20 +0100

    Installed json web token

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const express = require('express');
 
 const router = express.Router();
+const validateUser = require('../validation/user');
 
 
 module.exports = router
