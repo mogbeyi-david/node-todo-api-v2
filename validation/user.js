1df65b7557bba0f5f17c7ec16987f477766293ae
DavidMogbeyi <davidm@l5lab.com>
2019-02-22 07:32:57 +0100

    Add validation for a new user

---
+++ validation/user.js
@@ -0,0 +1,12 @@
+const Joi = require('joi');
+
+function validateNewUser (user) {
+  const schema = Joi.object().keys({
+    name: Joi.string().required(),
+    email: Joi.string().required().email,
+    password: Joi.string().required().regex(/^[a-zA-Z0-9]{3,30}$/)
+  });
+  return Joi.validate(user, schema);
+}
+
+module.exports = validateNewUser
