03bd48c87b34e56c24aa67af19be4d8dc2903670
DavidMogbeyi <davidm@l5lab.com>
2019-02-25 12:20:01 +0100

    Add testcase for the handler to get a single todo

--- tests/integration/todos.test.js
+++ tests/integration/todos.test.js
@@ -1,14 +1,63 @@
 let server
 const request = require('supertest')
+const Todo = require('../../models/todo')
 
 describe('/api/todos', () => {
   beforeEach(() => {server = require('../../app')})
-  afterEach(() => {server.close()})
+  afterEach(async () => {
+    server.close()
+    await Todo.remove({})
+  })
 
   describe('GET/', () => {
     it('should return all genres', async () => {
+      await Todo.collection.insertMany([
+        {
+          todo: 'first todo',
+          description: 'Description for first todo',
+          isComplete: false,
+          userId: '5c70a1cd68a83e13b39aae46'
+        },
+        {
+          todo: 'Second todo',
+          description: 'Description for Second todo',
+          isComplete: false,
+          userId: '5c70a1cd68a83e13b39aae46'
+        },
+      ])
       const response = await request(server).get('/api/todo/all')
       expect(response.status).toBe(200)
+      expect(response.body.length).toBe(2)
+      expect(response.body.some(t => t.todo === 'Second todo')).toBeTruthy()
+      expect(response.body.some(t => t.todo === 'first todo')).toBeTruthy()
+    })
+
+    it('should return a single todo if a valid ID is passed', async () => {
+      const todo = await new Todo({
+        todo: 'single todo',
+        description: 'Description for first todo',
+        isComplete: false,
+        userId: '5c70a1cd68a83e13b39aae46'
+      })
+      await todo.save()
+      const response = await request(server).get(`/api/todo/${todo._id}`)
+      expect(response.status).toBe(200)
+      expect(response.body.length).toBe(1)
+      expect(response.body.some(t => t.todo === 'single todo')).toBeTruthy()
+    })
+
+    it('should return a 404 error if an invalid ID is passed', async () => {
+
+      const id = '5c70a1cd68a83e13b39aae46'
+      const response = await request(server).get(`/api/todo/${id}`)
+      expect(response.status).toBe(404)
+      expect(response.body.message).toMatch(/No Todos Found/)
+    })
+
+    it('should return a 500 error if an improper ID is passed', async () => {
+      const id = 'xxx'
+      const response = await request(server).get(`/api/todo/${id}`)
+      expect(response.status).toBe(500)
     })
   })
 })
